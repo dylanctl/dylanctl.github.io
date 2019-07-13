666462db82cb7bdf3315c9f07054ffed7a62bf88
Dylan <macintosh@Dylan.local>
2019-07-13 17:27:43 +0800

    upload latest files

---
+++ script.js
@@ -0,0 +1,36 @@
+alert('Draco Dormiens Nunquam Titillandus');
+
+// let yearone = 2800*12;
+// let year23 = 3550*24;
+// let year4 = 4550*12;
+// let totalROI = yearone + year23 + year4;    
+
+// document.write(totalROI);
+
+// **Capitalizing the first letters of the string
+
+// let name = 'john wayne'
+
+// let splitName = name.split(' ')
+
+// for(i in splitName){
+//   splitName[i] = splitName[i].charAt(0).toUpperCase() + splitName[i].substring(1)
+// }
+
+// name = splitName.join(' ')
+
+// console.log(name)
+
+var num = "one two three";
+console.log(num.replace("two", "one.five"));
+
+// console.log("Hello world via console.log()");
+
+var firstname = "Sirius ";
+var lastname = "Black";
+var fullname = firstname + lastname;
+var length = fullname.length;
+console.log(length);
+console.log(fullname[(length/2)]);
+console.log(fullname.toLowerCase())
+
